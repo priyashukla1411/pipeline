656531f51ca5402113cf0218e6d69a52b60ac94e
priyashukla1411 <100112297+priyashukla1411@users.noreply.github.com>
2022-09-08 21:10:27 +0530

    Update cicd.tf

--- cicd.tf
+++ cicd.tf
@@ -1,4 +1,3 @@
-#################### VAR ####################
 variable "env" {
   description = "Depolyment environment"
   default     = "dev"
@@ -6,33 +5,31 @@ variable "env" {
 
 variable "github_branch" {
   description = "Repository branch to connect to"
-  default     = "main"
+  default     = "master"
 }
 
 variable "github_owner" {
   description = "GitHub repository owner"
   default     = "priyashukla1411"
 }
-
 variable "github_repo" {
   description = "GitHub repository name"
-  default     = "Django-1"
+  default     = "https://github.com/priyashukla1411/react-native.git"
 }
 
 variable "github_token" {
   description = "This is github authentication token"
-  default = "xxxxxxxxxxxx"
+  default = "**************"
   type = string
 }
 
+###############
 variable "artifacts_bucket_name" {
   description = "S3 Bucket for storing artifacts"
-  default     = "artifacts-bucket-devops1990"
+  default     = "artifacts-bucket-devops-rectnative1"
 }
-
-######################## S3 ##################
 resource "aws_s3_bucket" "artifacts_bucket_name" {
-  bucket = "artifacts-bucket-devops1990"
+  bucket = "artifacts-bucket-devops-rectnative1"
   acl    = "private"
 
   tags = {
@@ -40,9 +37,111 @@ resource "aws_s3_bucket" "artifacts_bucket_name" {
     Environment = "Dev"
   }
 }
-##################### pipeline ###################
-resource "aws_codepipeline" "node_pipeline" {
-  name     = "node_pipeline"
+##########################################################################################
+# code build:-
+
+data "template_file" "buildspec" {
+  template = "buildspec.yml"
+  
+}
+
+resource "aws_codebuild_project" "node_build" {
+  badge_enabled  = false
+  build_timeout  = 60
+  name           = "node_build"
+  queued_timeout = 480
+  service_role   = aws_iam_role.codebuild_role99888.arn
+  tags = {
+    Environment = var.env
+  }
+
+  artifacts {
+   
+    type                   = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:2.0"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = false
+    type                        = "LINUX_CONTAINER"
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      status = "ENABLED"
+    }
+
+    s3_logs {
+      encryption_disabled = false
+      status              = "DISABLED"
+    }
+  }
+
+  source {
+    type  = "GITHUB"
+    location = "https://github.com/priyashukla1411/react-native.git"
+    git_clone_depth = 1
+  }
+ 
+}
+
+resource "aws_iam_role" "codebuild_role99888" {
+  name = "reactnative"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_role_policy" "codebuild_policy" {
+  name = "nodepolicytest10"
+  role = aws_iam_role.codebuild_role99888.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+       
+        {
+            "Effect": "Allow",
+            "Resource": [
+                "arn:aws:s3:::${var.artifacts_bucket_name}/*"
+            ],
+            "Action": [
+                "*"
+            ]
+        },
+        {
+          "Effect": "Allow",
+          "Resource": [
+            "*"
+          ],
+          "Action": [
+            "*"
+          ]
+        }
+    ]
+}
+EOF
+}
+
+
+# # pipeline :-
+
+resource "aws_codepipeline" "node_pipeline11" {
+  name     = "node_pipeline11"
   role_arn = aws_iam_role.codepipeline_role.arn
   tags     = {
     Environment = var.env
@@ -61,14 +160,15 @@ resource "aws_codepipeline" "node_pipeline" {
       owner            = "ThirdParty"
       provider         = "GitHub"
       version          = "1"
-      input_artifacts  = []
+      # input_artifacts  = []
       output_artifacts = ["SourceArtifact"]
 
       configuration = {
-        Owner      = var.github_owner
-        Repo       = var.github_repo
-        Branch     = var.github_branch
-        OAuthToken = var.github_token
+        Owner      = "priyashukla1411"
+        Repo       = "react-native"  
+        Branch     = "master"
+        OAuthToken = "**************"
+ 
       }
     }
   }
@@ -115,17 +215,17 @@ resource "aws_codepipeline" "node_pipeline" {
         "BuildArtifact",
       ]
       name             = "Deploy"
-      output_artifacts = []
+   
       owner            = "AWS"
       provider         = "S3"
-      run_order        = 1
+
       version          = "1"
     }
   }
 }
 
 resource "aws_iam_role" "codepipeline_role" {
-  name = "noderoletest11"
+  name = "noderoletest1111"
 
   assume_role_policy = <<EOF
 {
@@ -143,7 +243,7 @@ resource "aws_iam_role" "codepipeline_role" {
 EOF
 }
 
-resource "aws_iam_role_policy" "codepipeline_policy" {
+resource "aws_iam_role_policy" "codepipeline_policy11" {
   name = "nodepolicytest11"
   role = aws_iam_role.codepipeline_role.id
 
@@ -168,108 +268,6 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
       "Resource": "*"
     }
   ]
-}
-EOF
-}
-
-################ Codebuild #################
-data "template_file" "buildspec" {
-  template = "buildspec.yml"
-  
-}
-
-resource "aws_codebuild_project" "node_build" {
-  badge_enabled  = false
-  build_timeout  = 60
-  name           = "node_build"
-  queued_timeout = 480
-  service_role   = aws_iam_role.codebuild_role.arn
-  tags = {
-    Environment = var.env
-  }
-
-  artifacts {
-    encryption_disabled    = false
-    name                   = "node_build-${var.env}"
-    override_artifact_name = false
-    packaging              = "NONE"
-    type                   = "CODEPIPELINE"
-  }
-
-  environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:2.0"
-    image_pull_credentials_type = "CODEBUILD"
-    privileged_mode             = false
-    type                        = "LINUX_CONTAINER"
-  }
-
-  logs_config {
-    cloudwatch_logs {
-      status = "ENABLED"
-    }
-
-    s3_logs {
-      encryption_disabled = false
-      status              = "DISABLED"
-    }
-  }
-
-  source {
-    buildspec           = data.template_file.buildspec.rendered
-    git_clone_depth     = 0
-    insecure_ssl        = false
-    report_build_status = false
-    type                = "CODEPIPELINE"
-  }
-}
-
-resource "aws_iam_role" "codebuild_role" {
-  name = "noderoletest10"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "codebuild.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-resource "aws_iam_role_policy" "codebuild_policy" {
-  name = "nodepolicytest10"
-  role = aws_iam_role.codebuild_role.id
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-       
-        {
-            "Effect": "Allow",
-            "Resource": [
-                "arn:aws:s3:::${var.artifacts_bucket_name}/*"
-            ],
-            "Action": [
-                "*"
-            ]
-        },
-        {
-          "Effect": "Allow",
-          "Resource": [
-            "*"
-          ],
-          "Action": [
-            "*"
-          ]
-        }
-    ]
 }
 EOF
 }
